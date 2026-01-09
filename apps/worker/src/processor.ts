6e66a5c17bcd36474640d2989c4ce63bfa32cf7a
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-01-09 21:26:02 +0530

    update database query functions [worker->processor.ts]

--- apps/worker/src/processor.ts
+++ apps/worker/src/processor.ts
@@ -12,15 +12,25 @@ import { scrapeWebsite } from "./services/scrape.service.js";
 
 export const processTaskJobs = async (taskId: string) => {
   try {
-    const task = await db.select().from(tasks).where(eq(tasks.id, taskId));
+    const task = await db.query.tasks.findFirst({
+      where: eq(tasks.id, taskId),
+      with: {
+        question: {
+          columns: {
+            url: true,
+            question: true,
+          }
+        }
+      }
+    });
     if (!task) throw new Error("Task not found.");
 
     await markProcessing(taskId);
 
-    const webContent = await scrapeWebsite(task[0].);
+    const webContent = await scrapeWebsite(task.question.url);
     console.log("webContent: ", webContent);
 
-    const aiAns = await askAI(webContent, task[0].question);
+    const aiAns = await askAI(webContent, task.question.question);
 
     // console.log("answer: ", aiAns);
 
