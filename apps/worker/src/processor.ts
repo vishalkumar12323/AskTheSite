befe739b74ef4552c1b0ecb7d2b161bb406b3bcd
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-05-25 21:53:48 +0530

    add redis pub/sub for real-time messaging

--- apps/worker/src/processor.ts
+++ apps/worker/src/processor.ts
@@ -10,6 +10,8 @@ import {
 import { askAI } from "./services/ai.service.js";
 import { scrapeWebsite } from "./services/scrape.service.js";
 
+import { redisConnection } from "./config/redis.js"
+
 export const processTaskJobs = async (taskId: string) => {
   try {
     const task = await db.query.tasks.findFirst({
@@ -30,15 +32,42 @@ export const processTaskJobs = async (taskId: string) => {
     if (!task) throw new Error("Task not found.");
 
     await markProcessing(taskId);
+    await redisConnection.publish(`task:${taskId}`, JSON.stringify({
+      status: "PROCESSING",
+      stage: "ANALYZING",
+      progress: 25
+    }));
 
     const webContent = await scrapeWebsite(task.question.url);
+    await redisConnection.publish(`task:${taskId}`, JSON.stringify({
+      status: "PROCESSING",
+      stage: "SCRAPING",
+      progress: 40
+    }));
+
 
     const aiAns = await askAI(webContent, task.question.question);
 
+    await redisConnection.publish(`task:${taskId}`, JSON.stringify({
+      status: "PROCESSING",
+      stage: "AI_THINKING",
+      progress: 60
+    }));
+
     await markCompleted(taskId, aiAns);
+
+    await redisConnection.publish(`task:${taskId}`, JSON.stringify({
+      status: "COMPLETED",
+      stage: "GENERATING",
+      progress: 100
+    }));
   } catch (error: any) {
     console.error("❌ Job failed:", error);
     await markFailed(taskId, error.message);
+    await redisConnection.publish(`task:${taskId}`, JSON.stringify({
+      status: "FAILED",
+      error: error.message
+    }));
 
     throw error;
   }
