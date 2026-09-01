6504ff086d5e25fa31af85cdfc6d317c2f4cdd56
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-09-01 17:36:57 +0530

    add ecs-execution-role and ecs-task-role for EcsStack!

--- apps/infrastructure/lib/ecs-stack.ts
+++ apps/infrastructure/lib/ecs-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 interface EcsStackProps extends cdk.StackProps {
     vpc: ec2.Vpc;
@@ -9,6 +10,9 @@ interface EcsStackProps extends cdk.StackProps {
 
 export class EcsStack extends cdk.Stack {
     public readonly cluster: ecs.Cluster;
+
+    public readonly executionRole: iam.Role;
+    public readonly taskRole: iam.Role;
 
     constructor(scope: Construct, id: string, props: EcsStackProps) {
         super(scope, id, props);
@@ -18,6 +22,19 @@ export class EcsStack extends cdk.Stack {
             clusterName: "askthesite-cluster",
         });
 
+        this.executionRole = new iam.Role(this, "EcsTaskExecutionRole", {
+            roleName: "ask-the-site-ecs-execution-role",
+            assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+
+            managedPolicies: [
+                iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy')
+            ]
+        });
+
+        this.taskRole = new iam.Role(this, "EcsTaskRole", {
+            roleName: "askthesite-ecs-task-role",
+            assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com')
+        });
 
         // Outputs
         new cdk.CfnOutput(this, "ClusterName", {
@@ -28,6 +45,14 @@ export class EcsStack extends cdk.Stack {
         new cdk.CfnOutput(this, "ClusterArn", {
             value: this.cluster.clusterArn,
             description: "AskTheSite ECS Cluster arn"
+        });
+
+        new cdk.CfnOutput(this, 'ExecutionRoleArn', {
+            value: this.executionRole.roleArn,
+        });
+
+        new cdk.CfnOutput(this, 'TaskRoleArn', {
+            value: this.taskRole.roleArn,
         });
     }
 };
