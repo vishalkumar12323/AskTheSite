c0fdecf681862b49e882dd299450f7e409f74012
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-09-01 22:03:46 +0530

    Add Task Definition for api, web, and worker services!

--- apps/infrastructure/lib/ecs-stack.ts
+++ apps/infrastructure/lib/ecs-stack.ts
@@ -3,9 +3,15 @@ import { Construct } from "constructs";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as logs from "aws-cdk-lib/aws-logs";
 
 interface EcsStackProps extends cdk.StackProps {
     vpc: ec2.Vpc;
+
+    apiRepository: ecr.Repository;
+    webRepository: ecr.Repository;
+    workerRepository: ecr.Repository;
 }
 
 export class EcsStack extends cdk.Stack {
@@ -35,6 +41,102 @@ export class EcsStack extends cdk.Stack {
             roleName: "askthesite-ecs-task-role",
             assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com')
         });
+
+        // API LogGroup
+        const apiLogGroup = new logs.LogGroup(this, "ApiLogGroup", {
+            logGroupName: "/ecs/askthesite/api",
+            retention: logs.RetentionDays.ONE_WEEK,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
+
+        // API Task Definition
+        const apiTaskDefinition = new ecs.FargateTaskDefinition(this, "ApiTaskDefinition", {
+            family: "askthesite-api",
+            cpu: 256,
+            memoryLimitMiB: 512,
+
+            executionRole: this.executionRole,
+            taskRole: this.taskRole
+        });
+
+        apiTaskDefinition.addContainer("ApiContainer", {
+            image: ecs.ContainerImage.fromEcrRepository(props.apiRepository, "v1"),
+            logging: ecs.LogDrivers.awsLogs({
+                streamPrefix: "api",
+                logGroup: apiLogGroup
+            }),
+            environment: {
+                NODE_ENV: "production",
+                PORT: "3001"
+            }
+        }).addPortMappings({
+            containerPort: 3001,
+            protocol: ecs.Protocol.TCP
+        });
+
+
+        // WEB LogGroup
+        const webLogGroup = new logs.LogGroup(this, "WebLogGroup", {
+            logGroupName: "/ecs/askthesite/web",
+            retention: logs.RetentionDays.ONE_WEEK,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
+
+        // WebTaskDefinition
+        const webTaskDefinition = new ecs.FargateTaskDefinition(this, "WebTaskDefinition", {
+            family: "askthesite-web",
+            cpu: 256,
+            memoryLimitMiB: 512,
+
+            executionRole: this.executionRole,
+            taskRole: this.taskRole
+        });
+
+        webTaskDefinition.addContainer("WebContainer", {
+            image: ecs.ContainerImage.fromEcrRepository(props.webRepository, "v1"),
+            logging: ecs.LogDrivers.awsLogs({
+                streamPrefix: "web",
+                logGroup: webLogGroup
+            }),
+            environment: {
+                NODE_ENV: "production",
+                PORT: "3000"
+            }
+        }).addPortMappings({
+            containerPort: 3000,
+            protocol: ecs.Protocol.TCP
+        });
+
+
+        // WORKER LogGroup
+        const workerLogGroup = new logs.LogGroup(this, "WorkerLogGroup", {
+            logGroupName: "/ecs/askthesite/worker",
+            retention: logs.RetentionDays.ONE_WEEK,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
+
+        // WorkerTaskDefinition
+        const workerTaskDefinition = new ecs.FargateTaskDefinition(this, "WorkerTaskDefinition", {
+            family: "askthesite-worker",
+            cpu: 256,
+            memoryLimitMiB: 512,
+
+            executionRole: this.executionRole,
+            taskRole: this.taskRole
+        });
+
+        workerTaskDefinition.addContainer("WorkerContainer", {
+            image: ecs.ContainerImage.fromEcrRepository(props.workerRepository, "v1"),
+            logging: ecs.LogDrivers.awsLogs({
+                streamPrefix: "worker",
+                logGroup: workerLogGroup
+            }),
+            environment: {
+                NODE_ENV: "production",
+            }
+        });
+
+
 
         // Outputs
         new cdk.CfnOutput(this, "ClusterName", {
