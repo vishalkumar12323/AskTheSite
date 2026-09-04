dcd13f66dfa82864d2c73d4aae0c353ad0b263ab
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-09-04 21:44:02 +0530

    change database connection url string to object that containing connection info!

--- apps/database/src/index.ts
+++ apps/database/src/index.ts
@@ -3,8 +3,14 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./db/schema.js"
 
-const client = postgres(process.env.DATABASE_URL!);
+const client = postgres({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
-export const db = drizzle({client, schema});
+export const db = drizzle({ client, schema });
 
 
