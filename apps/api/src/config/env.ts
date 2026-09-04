af2ffd207f134e06d44bd624a2cca0cddf88f20b
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-09-04 21:31:22 +0530

    change database connection url string!

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -2,6 +2,10 @@ import "dotenv/config";
 
 export const env = {
   PORT: Number(process.env.PORT) || 4000,
-  DATABASE_URL: process.env.DATABASE_URL!,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: Number(process.env.DB_PORT),
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
   REDIS_URL: process.env.REDIS_URL!,
 };
