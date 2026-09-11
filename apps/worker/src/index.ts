4a841fe647dec270bcea77ba9ca366169168f2b4
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-09-11 16:31:09 +0530

    chor(worker): add health-check server to worker index file!

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -2,8 +2,12 @@ import { Worker } from "bullmq";
 import { redisClient } from "./config/redis.js";
 import { processTaskJobs } from "./processor.js";
 import { logger } from "./logger/logger.js";
+import { startHealthServer } from "./health-server.js";
 
 const TASK_QUEUE_NAME = "task-queue";
+
+// Start lightweight HTTP server for health-check
+startHealthServer();
 
 new Worker(
   TASK_QUEUE_NAME,
